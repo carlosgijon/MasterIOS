444ebd9a0ff0c042dc453fc33116426b57b602ee
Carlos Gijon <carlosgijonfernandez@gmail.com>
2018-03-14 13:31:59 +0100

    Subida de nuevo archivo: Diferencia en la división

---
+++ Tema 2/DiferenciaDivision.playground/Contents.swift
@@ -0,0 +1,11 @@
+// No dan lo mismo las divisiones con enteros que con decimales
+
+import UIKit
+
+var x: Int = 5, y: Int = 12
+var relacion_ratio = y/x
+print("La relacion de ratio con enteros da \(relacion_ratio)")
+
+var fx: Float = 5, fy: Float = 12
+var relacion_ratio_con_floats: Float = fy/fx
+print("La relacion de ratio con decimales da \(relacion_ratio_con_floats)")
