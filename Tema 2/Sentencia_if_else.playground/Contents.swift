cfba21763e70436b832aceac276ba8068052f56b
Carlos Gijon <carlosgijonfernandez@gmail.com>
2018-03-21 15:32:44 +0100

    nueva subida

---
+++ Tema 2/Sentencia_if_else.playground/Contents.swift
@@ -0,0 +1,11 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+let edad = 32
+
+if (edad > 28) {
+    print("tienes \(edad) años, eres mayor de 28")
+}
+else {
+    print("tienes \(edad) años, eres menor o igual de 28")
+}
