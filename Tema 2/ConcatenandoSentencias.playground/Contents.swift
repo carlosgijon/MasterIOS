c4fd2a8a23b4261d003db340f6567ae52f971525
Carlos Gijon <carlosgijonfernandez@gmail.com>
2018-03-23 12:10:09 +0100

    nueva subida: concatenar expresiones

---
+++ Tema 2/ConcatenandoSentencias.playground/Contents.swift
@@ -0,0 +1,12 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var edad: Int = 32
+
+if(edad > 27 && edad < 35) {
+    print("Eres de la mejor generación!!!")
+}
+else {
+    print("Eres de una generación o bien de carcas o bien de ninis.")
+}
