74a8c6d1a7996e86994151ffc33eeeef02eb6fc0
Carlos Gijon <carlosgijonfernandez@gmail.com>
2018-03-23 12:29:28 +0100

    nueva subida: for in

---
+++ Tema 2/BucleFor.playground/Contents.swift
@@ -0,0 +1,9 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+let array_numero: [Int] = [1,2,3,4,5,6,7,8,9]
+
+for numeros in array_numero {
+    print(array_numero[numeros - 1])
+}
