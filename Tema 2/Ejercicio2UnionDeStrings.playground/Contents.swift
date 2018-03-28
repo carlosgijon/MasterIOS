f3c37ae457db954a060a1c8b186907257a878d10
Carlos Gijon <carlosgijonfernandez@gmail.com>
2018-03-28 11:56:03 +0200

    Nueva subida: Ejercicio juntar dos palabras en una frase

---
+++ Tema 2/Ejercicio2UnionDeStrings.playground/Contents.swift
@@ -0,0 +1,9 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+func devolverFrase(palabra1: String, palabra2: String) -> String {
+    return "\(palabra1) \(palabra2)"
+}
+
+print(devolverFrase(palabra1: "Hola", palabra2: "Carlos"))
