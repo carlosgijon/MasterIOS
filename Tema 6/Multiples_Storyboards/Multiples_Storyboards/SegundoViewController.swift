e88f5215901b569fae787e01c5c4c415341ec5f9
carlosgijon <carlosgijonfernandez@gmail.com>
2018-06-07 13:26:46 +0200

    nueva subida: múltiples Storyboard

---
+++ Tema 6/Multiples_Storyboards/Multiples_Storyboards/SegundoViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SegundoViewController.swift
+//  Multiples_Storyboards
+//
+//  Created by carlos on 7/6/18.
+//  Copyright © 2018 carlos. All rights reserved.
+//
+
+import UIKit
+
+class SegundoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Tema 6/Multiples_Storyboards/Multiples_Storyboards/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Multiples_Storyboards
+//
+//  Created by carlos on 7/6/18.
+//  Copyright © 2018 carlos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
